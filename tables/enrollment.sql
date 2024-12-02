04a1eb3d7dd93730d439cec9ec78bd58dfe11872
Benjamin <benjamin@zeroparallax.se>
2024-12-02 18:10:08 +0100

    Update for task 4

--- tables/enrollment.sql
+++ tables/enrollment.sql
@@ -3,4 +3,24 @@ INSERT INTO public.enrollment (lesson_id,person_id) VALUES
 	 (16,76),
 	 (16,53),
 	 (26,49),
-	 (26,75);
+	 (26,75),
+	 (26,20),
+	 (26,161),
+	 (26,26),
+	 (26,139),
+	 (25,26);
+INSERT INTO public.enrollment (lesson_id,person_id) VALUES
+	 (30,26),
+	 (30,49),
+	 (30,75),
+	 (30,20),
+	 (30,161),
+	 (30,139),
+	 (25,49),
+	 (25,75),
+	 (25,20),
+	 (25,161);
+INSERT INTO public.enrollment (lesson_id,person_id) VALUES
+	 (25,139),
+	 (25,76),
+	 (25,3);
